07ce0ee70ea3193e9146ac504b45f5b3942ddba4
Arafat <arafatmannan2019@gmail.com>
2024-08-25 22:49:02 +0600

    Monor heading fix2

--- src/app/_components/SparkleHeading.tsx
+++ src/app/_components/SparkleHeading.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export function SparklesPreview({ title, sparkle }: { title: string, sparkle?: boolean }) {
   return (
-    <div className={cn("h-[4rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md",
+    <div className={cn("h-[3rem] sm:h-[4rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md",
       sparkle && "h-[6rem]"
     )}>
       <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-white relative z-20">
