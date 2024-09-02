fe8c210b0afb17a01c04817f591c8bc74df2798d
Arafat <arafatmannan2019@gmail.com>
2024-09-03 00:30:37 +0600

    product card

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -21,7 +21,7 @@ export default function SidebarDemo({children}:{
   const links = [
     {
       label: "Products",
-      href: "#",
+      href: "/products",
       icon: (
         <FaProductHunt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
@@ -124,8 +124,8 @@ const Dashboard = ({children}:{
   children:ReactNode
 }) => {
   return (
-    <div className="flex flex-1 min-h-full overflow-y-auto">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+    <div className="flex flex-1 h-full">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full overflow-y-auto">
         {children}
       </div>
     </div>
--- src/app/(admin)/products/page.tsx
+++ src/app/(admin)/products/page.tsx
@@ -1,6 +1,8 @@
 import { SparklesPreview } from '@/app/_components/SparkleHeading'
 import { serverClient } from '@/app/_trpc/serverClient'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -16,7 +18,26 @@ async function Product() {
             <SparklesPreview title='All Products' />
             <div>
                 {
-                    products.length ? (<div className='w-full'>
+                    products.length ? (<div className='w-full grid gap-3 grid-cols-3 grid-row-1'>
+                        {
+                            products.map(e => {
+                                return (
+                                    <Card className='row-span-1 col-span-1' key={e.id}>
+                                        <CardHeader>
+                                            <CardTitle className='line-clamp-1'>
+                                                {e.name}
+                                            </CardTitle>
+                                            <CardDescription className='line-clamp-3'>
+                                                {e.description}
+                                            </CardDescription>
+                                            <CardContent>
+                                                <Image src={e.primaryImg} alt='product' width={250} height={500} className='rounded-[30px] mx-auto py-3'/>
+                                            </CardContent>
+                                        </CardHeader>
+                                    </Card>
+                                )
+                            })
+                        }
                     </div>) : (<div className='w-full'>
                         <p className='text-center'>No Product Found....</p>
                     </div>)
