1b6dcae0e60addd73c913a6e9ed009985186b8dc
root <root@server1.anhabd.com>
2024-11-06 04:27:12 +0000

    sshtest1

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className='font-sans text-3xl font-bold'>
-        <h1><span className='text-purple-700'>L</span><span>ogo</span></h1>
+        <h1><span className='text-purple-700'>A</span><span>nha</span></h1>
     </div>
   )
 }
